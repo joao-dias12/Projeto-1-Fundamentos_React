ac7900b5965770c50fa963614cb2fd30a2a6cb6b
Joao Dias <jpgd94@poli.ufrj.br>
2022-10-16 19:39:04 -0300

    "Outra forma de receber atributos"

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,9 +1,7 @@
 import styles from './Avatar.module.css'
 
 
-export function Avatar({hasBorder = true, src }){ // Nesse caso estamos definindo valores default tambem
-
-    const hasBorder = props.hasBorder !=false;
+export function Avatar({hasBorder = true, src }) { // Nesse caso estamos definindo valores default tambem
     return (
         <img 
         className={ hasBorder ? styles.avatarWithBorder : styles.avatar} // se props.hasBorder for true, faça isso(styles.avatarWithBorder) se falso, faça aquilo(styles.avatar) 
