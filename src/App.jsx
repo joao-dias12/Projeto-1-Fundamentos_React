4c554f8f6e8c06f80d5944c0af83cf39d80af4d7
Joao Dias <jpgd94@poli.ufrj.br>
2022-10-17 09:12:28 -0300

    "iteração de objetos posts dentro da interface"

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,45 @@ import { Header } from './components/header';
 import { Post }  from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
-import './global.css'
-
 import styles from './App.module.css';
 
+import './global.css'
+
+// author: { avatar_url: "", name : "", role ""}
+// publishedAt: Date
+// content: Str
+
+const posts = [
+  {
+    id: 1,
+    author:{
+      avatarUrl: 'https://media-exp1.licdn.com/dms/image/C4D03AQG0V_M9h40K0A/profile-displayphoto-shrink_800_800/0/1647860850052?e=1671062400&v=beta&t=ZmeCM9T4Z3PZWnvp18Ph7YVqKFN1bYMvRvSdU-vsh-0',
+      name: "João Dias",
+      role: 'Full Stack Junior'
+    },
+    content: [
+      {type: 'paragraph', content: '<p>Fala galeraa 👋'},
+      {type: 'paragraph', content : 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'paragraph', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2022-10-10 20:00:00')
+  },
+
+  {
+    id: 2,
+    author:{
+      avatarUrl: 'https://media-exp1.licdn.com/dms/image/C4E03AQE40-h6EJ9FyA/profile-displayphoto-shrink_400_400/0/1594823992253?e=1671667200&v=beta&t=dQ5SXvbShXfp3ww7uEw9TBSGsptcX_7uRFBGaKnNFu0',
+      name: "Giulia Folena",
+      role: 'Engenheira Ambiental'
+    },
+    content: [
+      {type: 'paragraph', content: '<p>Fala galeraa 👋'},
+      {type: 'paragraph', content : 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'paragraph', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2022-10-15 20:00:00')
+  }
+];
 export function App() {
   
   return (
@@ -18,16 +53,16 @@ export function App() {
         <Sidebar/>
         
         <main>
-          <Post
-            author="Diego Fernandes"
-            content=" Loremmmmm"
-        
-          />
-          <Post
-            author="João Dias"
-            content=" Novo Post"
-          
-          />
+         {posts.map(post => {
+          return (
+            <Post
+            author= {post.author}
+            content = {post.content}
+            publishedAt={post.publishedAt}
+
+            />
+          )
+         })}
         </main>
       </div>
      
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,15 +3,19 @@ import styles from "./Post.module.css"
 import { Avatar } from "./Avatar"
 
 
-export function Post() {
+export function Post(props) {
+
+    console.log(props)
     return (
         <article className={styles.post}> 
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://media-exp1.licdn.com/dms/image/C4D03AQG0V_M9h40K0A/profile-displayphoto-shrink_800_800/0/1647860850052?e=1671062400&v=beta&t=ZmeCM9T4Z3PZWnvp18Ph7YVqKFN1bYMvRvSdU-vsh-0"  />
+                    <Avatar src={props.author.avatarUrl}  /> {/* Colocando os atributos do objeto "props" na interface */}
+                    
+
                     <div className={styles.authorInfo}>
-                        <strong>João Dias</strong>
-                        <span>Web Developer</span>
+                        <strong>{props.author.name}</strong>
+                        <span>{props.author.role}</span>
                     </div>
                 </div>
 
